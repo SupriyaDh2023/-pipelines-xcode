28de9818f30a7f7e9717a2065e7e3beb445c33e7
SupriyaDh2023 <126596796+SupriyaDh2023@users.noreply.github.com>
2023-03-03 16:29:51 +0000

    Add files via upload

---
+++ iOSHelloWorld/iOSHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHelloWorldApp.swift
+//  iOSHelloWorld
+//
+//  Created by Supriya Subhash Dhamdhame on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
